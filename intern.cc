d1e1c7e5291f03ab22ac3befe9043d470b50425b
samson maben <samsonmaben@gmail.com>
2016-07-10 23:43:05 +0530

    updated

--- intern.cc
+++ intern.cc
@@ -1,57 +1,121 @@
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
 #include "ns3/mobility-module.h"
+#include "ns3/config-store-module.h"
+#include "ns3/wifi-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/applications-module.h"
-#include "ns3/wifi-module.h"
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
 using namespace ns3;
 
-int main(int argc,char *argv[])
+NS_LOG_COMPONENT_DEFINE ("WifiSimpleAdhoc");
+
+
+static void GenerateTraffic (Ptr<Socket> socket,Address address, uint32_t pktSize, 
+                             uint32_t pktCount,DataRate dataRate, Time pktInterval)
 {
-NodeContainer wifiNodes;
-wifiNodes.Create(3);
-
-YansWifiPhyHelper wifiPhy=YansWifiPhyHelper::Default();
-YansWifiChannelHelper wifiChannel=YansWifiChannelHelper::Default();
-
-wifiPhy.SetChannel(wifiChannel.Create());
-WifiHelper wifi=WifiHelper::Default();
-
-NqosWifiMacHelper wifiMac=NqosWifiMacHelper::Default();
-wifi.SetRemoteStationManager("ns3::ConstantRateWifiManager");
-wifiMac.SetType("ns3::AdhocWifiMac");
-
-NetDeviceContainer wifiDevices=wifi.Install(wifiPhy,wifiMac,wifiNodes);
-
-MobilityHelper mobility;
-mobility.SetPositionAllocator("ns3::GridPositionAllocator",
-"MinX",DoubleValue(0.0),
-"MinY",DoubleValue(0.0),
-"DeltaX",DoubleValue(5.0),
-"DeltaY",DoubleValue(10.0),
-"GridWidth",UintegerValue(3),
-"LayoutType",StringValue("RowFirst"));
-mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-mobility.Install(wifiNodes);
-
-
-Simulator::Run();
-Simulator::Destroy();
-return 0;
+  if (pktCount > 0)
+    {
+      socket->Send (Create<Packet> (pktSize));
+      Simulator::Schedule (pktInterval, &GenerateTraffic, address,
+                           socket, pktSize,pktCount-1,dataRate, pktInterval);
+    }
+  else
+    {
+      socket->Close ();
+    }
 }
 
 
+int main (int argc, char *argv[])
+{
+  std::string phyMode ("DsssRate1Mbps");
+  double rss = -80;  
+  uint32_t packetSize = 1000; 
+  uint32_t numPackets = 1;
+  double interval = 1.0; 
+  bool verbose = false;
 
+  CommandLine cmd;
 
+  cmd.AddValue ("phyMode", "Wifi Phy mode", phyMode);
+  cmd.AddValue ("rss", "received signal strength", rss);
+  cmd.AddValue ("packetSize", "size of application packet sent", packetSize);
+  cmd.AddValue ("numPackets", "number of packets generated", numPackets);
+  cmd.AddValue ("interval", "interval (seconds) between packets", interval);
+  cmd.AddValue ("verbose", "turn on all WifiNetDevice log components", verbose);
 
+  cmd.Parse (argc, argv);
 
+  Time interPacketInterval = Seconds (interval);
 
+  Config::SetDefault ("ns3::WifiRemoteStationManager::FragmentationThreshold", StringValue ("2200"));
+  
+  Config::SetDefault ("ns3::WifiRemoteStationManager::RtsCtsThreshold", StringValue ("2200"));
+  
+  Config::SetDefault ("ns3::WifiRemoteStationManager::NonUnicastMode", 
+                      StringValue (phyMode));
 
+  NodeContainer nodes;
+  nodes.Create (3);
 
+  
+  WifiHelper wifi;
+  if (verbose)
+    {
+      wifi.EnableLogComponents (); 
+    }
+  wifi.SetStandard (WIFI_PHY_STANDARD_80211b);
 
+  YansWifiPhyHelper wifiPhy =  YansWifiPhyHelper::Default ();
+  wifiPhy.Set ("RxGain", DoubleValue (0) ); 
+  wifiPhy.SetPcapDataLinkType (YansWifiPhyHelper::DLT_IEEE802_11_RADIO); 
 
+  YansWifiChannelHelper wifiChannel;
+  wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
+  wifiChannel.AddPropagationLoss ("ns3::FixedRssLossModel","Rss",DoubleValue (rss));
+  wifiPhy.SetChannel (wifiChannel.Create ());
 
+ 
+  WifiMacHelper wifiMac;
+  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
+                                "DataMode",StringValue (phyMode),
+                                "ControlMode",StringValue (phyMode));
+  
+  wifiMac.SetType ("ns3::AdhocWifiMac");
+  NetDeviceContainer devices = wifi.Install (wifiPhy, wifiMac, nodes);
 
+  InternetStackHelper internet;
+  internet.Install (c);
 
+  Ipv4AddressHelper ipv4;
+  NS_LOG_INFO ("Assign IP Addresses.");
+  ipv4.SetBase ("10.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer i = ipv4.Assign (devices);
+
+   uint16_t sinkPort = 8080;
+  Address sinkAddress (InetSocketAddress (interfaces.GetAddress (1), sinkPort));
+  PacketSinkHelper packetSinkHelper ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), sinkPort));
+  ApplicationContainer sinkApps = packetSinkHelper.Install (nodes.Get (1));
+  sinkApps.Start (Seconds (50.0));
+  sinkApps.Stop (Seconds (150.0));
+
+  Ptr<GenerateTraffic> app = CreateObject<GenerateTraffic> ();
+  app->Setup (ns3TcpSocket, sinkAddress, 1040, 1000, DataRate ("1Mbps"));
+  nodes.Get (0)->AddApplication (app);
+  app->SetStartTime (Seconds (50.0));
+  app->SetStopTime (Seconds (150.));
+
+ 
+  Simulator::Stop (Seconds(150));
+  Simulator::Run ();
+  Simulator::Destroy ();
+
+  return 0;
+}
 
